022f90390d40708089f5ae92344d043ce0c95c5b
thebromoe <kebbi@ualberta.ca>
2018-11-17 13:21:54 -0700

    add post request

--- server/server.py
+++ server/server.py
@@ -8,6 +8,14 @@ pip3 install flask
 from flask import Flask
 app = Flask(__name__)
 
+
+# GET ROUTE
 @app.route("/")
 def hello():
     return "Hello World!"
+
+# POST ROUTE
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'This is my Post: %d' % post_id
