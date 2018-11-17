643c8a06e99a879e46680a2b118f51c6a31c0c5c
Evan <timmsevan@gmail.com>
2018-11-17 15:41:39 -0700

    updated server

--- server/server.py
+++ server/server.py
@@ -8,11 +8,23 @@ RUN SERVER:
 '''
 
 from flask import Flask, request
+import json
 app = Flask(__name__)
 
 
 # GET/POST ROUTE
 @app.route('/', methods=['GET', 'POST'])
 def parse_request():
-    data = request.data  
+     
     
+    if request.method == 'GET':
+        return "Hello"
+    elif request.method == 'POST':
+        print(request.json)
+        return json.dumps(request.json)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
