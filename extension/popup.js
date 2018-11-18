7254bf2be43611e621d47258c1dfc16e2d26c0f4
Song Hui Zhang <songhui@ualberta.ca>
2018-11-17 22:22:01 -0700

    popup placeholders

--- extension/popup.js
+++ extension/popup.js
@@ -2,17 +2,35 @@
 
 chrome.runtime.sendMessage('init', function(response){
     var words = response.responses[0].textAnnotations[0].description
-    
+    var submit = document.createElement('input');
+    submit.setAttribute('id', 'submit-untrans');
+    submit.setAttribute('type', 'submit');
+
     words.trim().split('\n').map(function(item) {
         console.log(item);
+        document.getElementById('word-list').appendChild(newBar(item));
     });
+    document.getElementById('word-list').appendChild(submit);
+
+    document.getElementById('submit-untrans').addEventListener('click', submitUntranslated);
 });
 
-function addElement() {
-    var p = document.createElement('p');
-    var n = document.createTextNode('this is text');
-    p.appendChild(n);
-    document.getElementById('word-list').appendChild(p);
+function newBar(item) {
+    var element = document.createElement('div');
+    var input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('value', item);
+
+    element.appendChild(input);
+    element.appendChild(document.createElement('button'));
+    element.appendChild(document.createElement('button'));
+    element.appendChild(document.createElement('button'));
+    element.appendChild(document.createElement('button'));
+    element.appendChild(document.createElement('br'));
+
+    return element;
 }
 
-document.getElementById('alert').addEventListener('click', addElement);
+function submitUntranslated(event) {
+    console.log(event);
+}
